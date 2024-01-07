d10ff970208b87f34e8f3de43fed48251b8a56f0
AniolENTI <aniol.morato@enti.cat>
2024-01-07 14:46:09 +0100

    Començat a treballar en la visibilitat de l'api

--- ERRT_App/app/src/main/java/com/enti/dostres/cdi/arnauvilar/aniolmorato/eldenring/fragments/screens/ItemsScreen.kt
+++ ERRT_App/app/src/main/java/com/enti/dostres/cdi/arnauvilar/aniolmorato/eldenring/fragments/screens/ItemsScreen.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.R
+import com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.ItemAdapter
+import com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.ItemProvider
+import com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.Repositories.ItemApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ItemsScreen: Fragment() {
+class ItemsScreen : Fragment() {
+
+    private lateinit var table: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +27,29 @@ class ItemsScreen: Fragment() {
         return inflater.inflate(R.layout.screen_items, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        table = view.findViewById(R.id.my_items_table)
+        table.layoutManager = LinearLayoutManager(requireContext())
+
+        val repository = ItemApiService()
+        val provider = ItemProvider(repository)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val heroes = provider.GetAllItems()
+
+            CoroutineScope(Dispatchers.Main).launch {
+                table.adapter = ItemAdapter(heroes)
+            }
+        }
+    }
 }
+
+    /*override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.screen_items, container, false)
+    }*/
