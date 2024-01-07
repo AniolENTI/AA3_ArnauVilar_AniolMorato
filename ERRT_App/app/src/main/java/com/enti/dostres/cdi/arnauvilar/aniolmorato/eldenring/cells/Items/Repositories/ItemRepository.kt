71e496dac233c4fbb14cd223fab45f3e9a5c59ff
AniolENTI <aniol.morato@enti.cat>
2024-01-07 12:57:52 +0100

    ItemRepository i ItemResponse

--- ERRT_App/app/src/main/java/com/enti/dostres/cdi/arnauvilar/aniolmorato/eldenring/cells/Items/Repositories/ItemRepository.kt
+++ ERRT_App/app/src/main/java/com/enti/dostres/cdi/arnauvilar/aniolmorato/eldenring/cells/Items/Repositories/ItemRepository.kt
@@ -1,6 +1,6 @@
 package com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.Repositories
 
-import ItemData
+import com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.ItemData
 
 interface ItemRepository {
     suspend fun GetItems() : MutableList<ItemData>
---
+++ ERRT_App/app/src/main/java/com/enti/dostres/cdi/arnauvilar/aniolmorato/eldenring/cells/Items/Repositories/ItemResponse.kt
@@ -0,0 +1,8 @@
+package com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.Repositories
+
+import com.enti.dostres.cdi.arnauvilar.aniolmorato.eldenring.cells.Items.ItemData
+import com.google.gson.annotations.SerializedName
+
+data class ItemResponse(val code: Int, @SerializedName("data") val itemData: ItemResponseData)
+
+data class ItemResponseData(val offset: Int, val count: Int, val total: Int, @SerializedName("results") val heroList: MutableList<ItemData>)
